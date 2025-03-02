505e48f7fd7211b16b36fcfbb3f6f1c57dbffa70
dljardim <dljardim@outlook.com>
2025-03-01 21:17:20 -0600

    Initial Commit

---
+++ SwiftUIExamples/SwiftUIExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIExamplesApp.swift
+//  SwiftUIExamples
+//
+//  Created by Damian Jardim on 3/1/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
